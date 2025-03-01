0a7908eead12560ee078e7d4bb063873b47ab8eb
Katelyn <katelyncicifu@gmail.com>
2025-02-28 19:00:42 -0500

    Update testscript.py

--- testscript.py
+++ testscript.py
@@ -2,7 +2,7 @@ import os
 def testdownload():
   path = input("What is the path to the dataset? ")
   assert os.path.exists(path)
-  return "You have downloaded the dataset!" 
+  print("You have downloaded the dataset!")
 
 
 testdownload()
