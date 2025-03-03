54b8a79e4e5fec19545fa4b5caeecdbb5e542980
Katelyn <katelyncicifu@gmail.com>
2025-03-03 17:13:34 -0500

    Update testscript.py fixed typos

--- testscript.py
+++ testscript.py
@@ -1,9 +1,9 @@
 import os
 import unittest
 
-path = os.path.expanduser("~/CDS/Tio_spotify_songs.csv")
+path = os.path.expanduser("~/CDS/Top_spotify_songs.csv")
 def testdownload():
-  assertTrue(os.path.exists(path))
+  assert(os.path.exists(path))
   return "You have downloaded the dataset correctly!"
 
 
