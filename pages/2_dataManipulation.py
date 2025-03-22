86cfc897764e10c063924721b0c33c288c7aaefa
Rahi Dasgupta <rahidasgupta04@gmail.com>
2025-03-22 08:48:37 -0400

    Created Frontend Strealit application describing our project - rahi dasgupta

---
+++ pages/2_dataManipulation.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Data Manipulation")
+
+st.write("We decided to clean all the missing values out of the dataset and replace all the NaN values with a summary statistic. We did it this way because it made for not losing a lot of data so our dataset would be more complete. ")
