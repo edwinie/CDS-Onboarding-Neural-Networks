5491c72b45d90d3457d2dc268b5e5b051d1808ce
edwinie <edwinlincollege@gmail.com>
2025-03-22 09:45:18 -0400

    moved the path checking test case

--- ms2_testscript.py
+++ ms2_testscript.py
@@ -4,15 +4,6 @@ import pandas as pd
 import numpy as np
 from pandas.testing import assert_frame_equal
 
-path = os.path.join(".", "Top_spotify_songs.csv")
-print(path)
-def testdownload():
-  assert(os.path.exists(path)), "ERROR: Dataset not found at " + path + ". Please download it."
-  print("Dataset found")
-  return True
-
-testdownload()
-
 def removenanrows(dataset):
     return dataset.dropna()
 
