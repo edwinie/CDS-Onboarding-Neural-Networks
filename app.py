2993efbd496bb51b304913f93d6bd326cffddb02
Katelyn <katelyncicifu@gmail.com>
2025-04-18 23:20:30 -0400

    Update app.py, changed header name

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.title("The Neural Network - Katelyn, Rahi, & Edwin")
-    st.header("Next Top Songs")
+    st.header("NextTrack")
     
 
 if __name__ == "__main__":
